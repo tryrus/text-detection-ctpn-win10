c8913b5d67b8684f347d08af323835f60d7b513f
tryrus <tryrus@163.com>
2019-08-25 21:22:30 +0800

    init repo

---
+++ utils/bbox/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+import numpy as np
+from Cython.Build import cythonize
+from distutils.extension import Extension
+
+numpy_include = np.get_include()
+setup(ext_modules=cythonize("bbox.pyx"),include_dirs=[numpy_include])
+setup(ext_modules=cythonize("nms.pyx"),include_dirs=[numpy_include])
